222d8f627fe2211cfd1184e88b6b25a6abf780bd
Mmasehume Raphiri <2089198@students.wits.ac.za>
2021-08-29 22:45:40 +0200

    updated tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:aishop/main.dart';
+
 void main() {
-  testWidgets('email or password is empty, does not sign in', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
 
-    bool didSignIn = false;
-    expect(didSignIn, false);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
-
-  testWidgets('non-empty email and password, valid account, call sign in, succeed', (WidgetTester tester) async {
-
-    bool didSignIn = true;
-    expect(didSignIn, true);
-
-  });
-
 }
